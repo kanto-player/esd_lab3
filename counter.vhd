0c2c06c6ea0c3269d14a9674ba879bdd2261329d
Zhehao Mao <zhehao.mao@gmail.com>
2013-03-15 10:53:09 -0400

    fix trailing semicolon

--- counter.vhd
+++ counter.vhd
@@ -3,10 +3,8 @@ use ieee.std_logic_1164.all;
 use ieee.numberic_std.all;
 
 entity counter is
-    port (
-        clk     : in std_logic;
-        count   : out unsigned(9 downto 0);
-    );
+    port (clk   : in std_logic;
+          count : out unsigned(9 downto 0));
 end counter;
 
 architecture rtl of counter is
--- frequency_divider.vhd
+++ frequency_divider.vhd
@@ -10,11 +10,9 @@ use ieee.numberic_std.all;
 --          inserting 2 as divider_count divides by 4
 --          inserting 3 as divider_count divides by 6, etc.
 entity frequency_divider is
-    port (
-        clk         : in std_logic;
-        divider     : in std_logic_vector(9 downto 0);
-        clk_out     : out std_logic;
-    );
+    port (clk     : in std_logic;
+          divider : in std_logic_vector(9 downto 0);
+          clk_out : out std_logic);
 end frequency_divider;
 
 architecture rtl of frequency_divider is
